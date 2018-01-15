fd3022dd2f16ed0b3d6926a55f375dad5168a837
Melissa Donahue <msue1282@gmail.com>
2018-01-15 18:52:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 
 var name = "Janet"
-var height = 74
+var height = "74"
 
 
 console.log("Name:")
@@ -9,3 +9,4 @@ console.log(name)
 
 console.log("Height:")
 console.log(height + 1)
+console.log(parseInt(height) + 1 )
